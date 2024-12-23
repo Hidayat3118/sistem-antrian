678d6dba5d607b0cdb31b265306ac35521c34361
Abu Husein <huseinabu01@gmail.com>
2024-12-23 23:04:56 +0800

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Admin;
 use App\Models\Antrian;
+use App\Models\Rekap;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -17,11 +18,43 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // Admin::create([
-        //     'nama' => 'Abu Husein',
-        //     'username' => 'Husein',
-        //     'password' => Hash::make('1234'),
-        // ]);
+        Admin::create([
+            'nama' => 'Abu Husein',
+            'username' => 'Husein',
+            'password' => Hash::make('1234'),
+        ]);
+
+        $rekaps = [
+            [
+                'tanggal' => '2024-11-20',
+                'jmblh_umum' => 20,
+                'jmblh_prioritas' => 12,
+                'tdk_dilayani' => 5,
+            ],
+            [
+                'tanggal' => '2024-1-2',
+                'jmblh_umum' => 21,
+                'jmblh_prioritas' => 2,
+                'tdk_dilayani' => 2,
+            ],
+            [
+                'tanggal' => '2024-12-2',
+                'jmblh_umum' => 19,
+                'jmblh_prioritas' => 4,
+                'tdk_dilayani' => 0,
+            ],
+            [
+                'tanggal' => '2024-12-18',
+                'jmblh_umum' => 30,
+                'jmblh_prioritas' => 18,
+                'tdk_dilayani' => 7,
+            ],
+        ];
+
+        foreach ($rekaps as $rekap){
+            Rekap::create($rekap);
+        }
+        
 
         for ($i = 1; $i < 10; $i++) {
             Antrian::create([
