ec5b4e559a1866875604fa4aedc320a0787424bd
Abu Husein <huseinabu01@gmail.com>
2025-03-21 15:12:29 +0800

    add admin middleware

--- routes/web.php
+++ routes/web.php
@@ -41,23 +41,23 @@ Route::get('/admin/login', [adminController::class, 'viewLogin'])->name('login')
 
 Route::post('/admin/login', [adminController::class, 'login']);
 
-Route::post('/admin/logout', [adminController::class, 'logout'])->middleware('auth');
+Route::post('/admin/logout', [adminController::class, 'logout'])->middleware('admin');
 
-Route::put('/admin/edit/{admin}', [adminController::class, 'update']);
+Route::put('/admin/edit/{admin}', [adminController::class, 'update'])->middleware('admin');
 
-Route::get('/admin/loketSatu', [antrianCrontroller::class, 'loketUmum']);
+Route::get('/admin/loketSatu', [antrianCrontroller::class, 'loketUmum'])->middleware('admin');
 
-Route::get('/admin/loketDua', [antrianCrontroller::class, 'loketPrioritas']);
+Route::get('/admin/loketDua', [antrianCrontroller::class, 'loketPrioritas'])->middleware('admin');
 
-Route::get('admin/rekap', [rekapController::class, 'index']);
+Route::get('admin/rekap', [rekapController::class, 'index'])->middleware('admin');
 
 Route::get('admin/profil', function () {
     return view('admin.profil', [
         'title' => 'Admin | Profile',
         'active' => 'profile',
     ]);
-});
+})->middleware('admin');
 
-Route::put('antrian/selesai/{antrian}', [antrianCrontroller::class, 'selesai']);
+Route::put('antrian/selesai/{antrian}', [antrianCrontroller::class, 'selesai'])->middleware('admin');
 
-Route::put('antrian/terlewat/{antrian}', [antrianCrontroller::class, 'terlewat']);
+Route::put('antrian/terlewat/{antrian}', [antrianCrontroller::class, 'terlewat'])->middleware('admin');
